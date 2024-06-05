70de08739aa165dcb977cb0189e88962a2bb45b8
AnuragiTriangle <anurag@itriangle.in>
2024-06-05 15:34:21 +0530

    adding customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-}
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import CustomersTable from '@/app/ui/customers/table';
+import { CreateCustomer } from '@/app/ui/customers/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+
+    const customers = await fetchCustomers();
+
+    return (
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Search customers..." />
+          <CreateCustomer />
+        </div>
+        <Suspense fallback={<CustomersTableSkeleton />}>
+          <CustomersTable customers={customers}/>
+        </Suspense>
+        {/* <div className="mt-5 flex w-full justify-center">
+          <Pagination totalPages={totalPages} />
+        </div> */}
+      </div>
+    );
+  }
